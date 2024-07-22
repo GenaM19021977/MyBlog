f20c0948ebfb2b581c8532a8ab41701a46bab169
malch <malchewski@mail.ru>
2024-07-22 20:39:19 +0300

    Создание моделей и индексов

--- mysites/blog/models.py
+++ mysites/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):  # Добавление поля статус
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')  # Добавление поля автора, связь с моделью User
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)  # Добавление поля статуса
+
+    class Meta:
+        ordering = ['-publish']  # Добавление отображения по убыванию жаты публикации
+        indexes = [
+            models.Index(fields=['-publish']),  # Добавление индексации
+        ]
+
+    def __str__(self):
+        return self.title
